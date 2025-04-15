8f0e6c7fcfe3e21b699886d93df9e810110eacfd
Helena Madrenys Planas <hmadrenys@gmail.com>
2025-04-15 22:57:54 +0200

    First iteration rotation

--- Assets/Scripts/Ovella/MoveToPositionAction.cs
+++ Assets/Scripts/Ovella/MoveToPositionAction.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
 using DG.Tweening;
+using System.Runtime.CompilerServices;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "MoveAgent", story: "Moves [Self] to [TargetPosition] and changes [IsMoving] value", category: "Action", id: "7523f7c4f63362e7c94073fec6fcaafa")]
-public partial class MoveAgentAction : Action
+[NodeDescription(name: "MoveToPosition", story: "Move [Self] to [TargetPosition]", category: "Action", id: "3391ffcaadad1b7d59f2b72bf3d0300f")]
+public partial class MoveToPositionAction : Action
 {
-    [SerializeReference] public BlackboardVariable<GameObject> Agent;
-    [SerializeReference] public BlackboardVariable<Vector3> Location;
-    [SerializeReference] public BlackboardVariable<Vector3> LocationPadding;
+    [SerializeReference] public BlackboardVariable<GameObject> Self;
+    [SerializeReference] public BlackboardVariable<Vector3> TargetPosition;
     [SerializeReference] public BlackboardVariable<float> Speed = new BlackboardVariable<float>(1.0f);
     [SerializeReference] public BlackboardVariable<float> DistanceThreshold = new BlackboardVariable<float>(0.2f);
     [SerializeReference] public BlackboardVariable<float> SlowDownDistance = new BlackboardVariable<float>(1.0f);
 
+    private bool firstUpdate = true;
+
     protected override Status OnStart()
     {
-        if (Agent.Value == null || Location.Value == null)
+        if (Self.Value == null || TargetPosition.Value == null)
         {
             return Status.Failure;
         }
@@ -28,8 +30,9 @@ public partial class MoveAgentAction : Action
 
     protected override Status OnUpdate()
     {
-        if (Agent.Value == null || Location.Value == null)
+        if (Self.Value == null || TargetPosition.Value == null)
         {
+            firstUpdate = true;
             return Status.Failure;
         }
 
@@ -37,6 +40,7 @@ public partial class MoveAgentAction : Action
         float distance = GetDistanceToLocation(out agentPosition, out locationPosition);
         if (distance <= DistanceThreshold)
         {
+            firstUpdate = true;
             return Status.Success;
         }
 
@@ -52,34 +56,35 @@ public partial class MoveAgentAction : Action
         toDestination.y = 0.0f;
         toDestination.Normalize();
         agentPosition += toDestination * (speed * Time.deltaTime);
-        Agent.Value.transform.position = agentPosition;
+        Self.Value.transform.position = agentPosition;
 
         // Look at the target.
-        //Agent.Value.transform.forward = toDestination;
-        float angle = Vector2.SignedAngle(toDestination, Vector2.up);
-        Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
-        Agent.Value.transform.rotation = Quaternion.Lerp(Agent.Value.transform.rotation, targetRotation, 0.05f * Time.deltaTime);
+        //Self.Value.transform.forward = toDestination;
+        if (firstUpdate)
+        {
+            float angle = Vector3.SignedAngle(toDestination, agentPosition, Vector3.up);
+            Self.Value.transform.DOLocalRotate(new Vector3(0, angle, 0), 0.3f);
+            firstUpdate = false;
+        }
 
         return Status.Running;
     }
-
 
     private Status Initialize()
     {
         if (GetDistanceToLocation(out Vector3 agentPosition, out Vector3 locationPosition) <= DistanceThreshold)
         {
+            firstUpdate = true;
             return Status.Success;
         }
+
         return Status.Running;
     }
 
     private float GetDistanceToLocation(out Vector3 agentPosition, out Vector3 locationPosition)
     {
-        agentPosition = Agent.Value.transform.position;
-        locationPosition = Location.Value + LocationPadding;
-
-        Debug.Log("AgentPosition: " + agentPosition);
-        Debug.Log("LocationPosition: " + locationPosition);
+        agentPosition = Self.Value.transform.position;
+        locationPosition = TargetPosition.Value;
         return Vector3.Distance(new Vector3(agentPosition.x, locationPosition.y, agentPosition.z), locationPosition);
     }
 }
