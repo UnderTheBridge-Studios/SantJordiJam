1fe482b0807e4d0e432bfe573bea0c5cd1c6dcc0
Diego_Iglesias <diego.i@live.com>
2025-04-20 21:47:45 +0200

    fix gm

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -191,7 +191,7 @@ public class GameManager : MonoBehaviour
         {
             yield return new WaitForSeconds(Random.Range(minTimeBetweenClients, maxTimeBetweenClients));
             if (m_clientManagerRef.GetClientCount() < m_MaxClients)
-                m_clientManagerRef.SpawnClientTutorial();
+                m_clientManagerRef.TrySpawnClient();
 
             if (m_DayCount > 3)
                 m_MaxClients = 4;
